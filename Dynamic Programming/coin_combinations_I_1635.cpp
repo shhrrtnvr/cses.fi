c648b5e9a8821644aa10f2892d36965e7f817655
Ahmed Shahriar Tanvir <shhrrtnvr@gmail.com>
2021-04-29 18:03:26 +0600

    shortest routes I ac

--- Dynamic Programming/coin_combinations_I_1635.cpp
+++ Dynamic Programming/coin_combinations_I_1635.cpp
@@ -3,20 +3,20 @@ using namespace std;
 #define MX 1000007
 #define MOD 1000000007
 
-int coin_count[MX];
+int ways_count[MX];
 
 int main() {
     int n, x;
     scanf("%d %d", &n, &x);
     int coins[n];
     for (int i = 0; i < n; i++) scanf("%d", &coins[i]);
-    coin_count[0] = 1;
+    ways_count[0] = 1;
     for (int i = 0; i < x; i++) {
         for (auto val : coins) {
             if (i+val > x) continue;
-            coin_count[i+val] = (coin_count[i+val] + coin_count[i]) % MOD;
+            ways_count[i+val] = (ways_count[i+val] + ways_count[i]) % MOD;
         }
     }
-    printf("%d\n", coin_count[x]);
+    printf("%d\n", ways_count[x]);
     return 0;
 }
---
+++ Graph Algorithms/shortest_routes_I_1671.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 0x3f3f3f3f
+ 
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    long long dist[n+1];
+    memset(dist, INF, sizeof dist);
+    dist[1] = 0;
+    vector<pair<int, long long> > AdjList[n+1];
+    for (int i = 0, a, b, c; i < m; i++) {
+        scanf("%d %d %d", &a, &b, &c);
+        AdjList[a].emplace_back(make_pair(b, c));
+    }
+
+    priority_queue<pair<long long, int>, vector<pair<long long, int> >, greater<pair<long long, int> > > pq;
+    bool processed[n+1];
+    for (int i = 0; i <= n; i++) processed[i] = false;
+    dist[1] = 0;
+    pq.push(make_pair(0, 1));
+    while (!pq.empty()) {
+        auto [du, u] = pq.top(); pq.pop();
+        if (processed[u]) continue;
+        processed[u] = true;
+        for (auto [v, c] : AdjList[u]) {
+            if (du + c < dist[v]) {
+                dist[v] = du + c;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i-1) printf(" ");
+        printf("%lld", dist[i]);
+    }
+    printf("\n");
+    return 0;
+}
